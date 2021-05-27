0f40f40e520c7dff4ae3572f56684cca228c7ce9
Jeroen Dekker <jeroen@ytec.nl>
2021-05-27 17:11:10 +0200

    Add version 0.7.0

--- src/bem-toolkit.bem-nodelist.ts
+++ src/bem-toolkit.bem-nodelist.ts
@@ -2,106 +2,107 @@
  * An array of BEM elements with some advanced functions
  */
 export default class BEMNodeList extends Array<Element> {
-  public static create<T>(): BEMNodeList {
-    return Object.create(BEMNodeList.prototype) as BEMNodeList;
-  }
+    private _blockName: string;
 
-  /**
-   * sets innerHTML for all elements in the BEMNodeList
-   */
-  public set innerHTML(HTML: string) {
-    this.forEach((element) => {
-      element.innerHTML = HTML; // eslint-disable-line no-param-reassign
-    });
-  }
+    public static create<T>(): BEMNodeList {
+        return Object.create(BEMNodeList.prototype) as BEMNodeList;
+    }
 
-  /**
-   * Returns a BEMNodelist containing all elements that have a certain BEM modifier
-   * @param modifier BEM modifier string
-   */
-  public withModifier(modifier: string): BEMNodeList {
-    const nodes = new BEMNodeList();
-    this.forEach((element: Element) => {
-      if (element.getBEMModifiers().includes(modifier)) nodes.push(element);
-    });
+    public set blockName(name: string) {
+        if (typeof this._blockName === 'undefined') {
+            this._blockName = name;
+        }
+    }
 
-    return nodes;
-  }
+    public get blockName(): string {
+        return this._blockName;
+    }
 
-  /**
-   * Adds an event listener to all elements in the BEMNodeList
-   * @param type A case-sensitive string representing the event type to listen for.
-   * @param listener Callback that is called when the event occurs
-   * @param useCapture A Boolean indicating whether events of this type will be dispatched to the registered listener before being dispatched to any EventTarget beneath it in the DOM tree.
-   */
-  public addEventListener(
-    type: string,
-    listener: (event?: Event) => void,
-    useCapture = false
-  ): void {
-    this.forEach((element) => {
-      element.addEventListener(type, listener, useCapture);
-    });
-  }
+    /**
+     * sets innerHTML for all elements in the BEMNodeList
+     */
+    public set innerHTML(HTML: string) {
+        this.forEach((element) => {
+            element.innerHTML = HTML; // eslint-disable-line no-param-reassign
+        });
+    }
 
-  /**
-   * Removes an event listener from all elements in the BEMNodeList
-   * @param type A case-sensitive string representing the event type to listen for.
-   * @param listener The callback that is to be removed
-   * @param useCapture A Boolean indicating whether events of this type will be dispatched to the registered listener before being dispatched to any EventTarget beneath it in the DOM tree.
-   */
-  public removeEventListener(
-    type: string,
-    listener: (event?: Event) => void,
-    useCapture = false
-  ): void {
-    this.forEach((element) => {
-      element.removeEventListener(type, listener, useCapture);
-    });
-  }
+    /**
+     * Returns a BEMNodelist containing all elements that have a certain BEM modifier
+     * @param modifier BEM modifier string
+     */
+    public withModifier(modifier: string): BEMNodeList {
+        const nodes = new BEMNodeList();
+        this.forEach((element: Element) => {
+            if (element.getBEMModifiers(this._blockName).includes(modifier)) nodes.push(element);
+        });
 
-  /**
-   * Toggles a BEM Modifier on or off for all elements of the BEMNodeList
-   */
-  public toggleBEMModifier(modifier: string, force?: boolean): void {
-    this.forEach((element) => {
-      element.toggleBEMModifier(modifier, force);
-    });
-  }
+        return nodes;
+    }
 
-  /**
-   * Adds a BEM Modifier for all elements of the BEMNodeList
-   */
-  public addBEMModifier(modifier: string): void {
-    this.forEach((element) => {
-      element.addBEMModifier(modifier);
-    });
-  }
+    /**
+     * Adds an event listener to all elements in the BEMNodeList
+     * @param type A case-sensitive string representing the event type to listen for.
+     * @param listener Callback that is called when the event occurs
+     * @param useCapture A Boolean indicating whether events of this type will be dispatched to the registered listener before being dispatched to any EventTarget beneath it in the DOM tree.
+     */
+    public addEventListener(type: string, listener: (event?: Event) => void, useCapture = false): void {
+        this.forEach((element) => {
+            element.addEventListener(type, listener, useCapture);
+        });
+    }
 
-  /**
-   * Removes a BEM Modifier for all elements of the BEMNodeList
-   */
-  public removeBEMModifier(modifier: string): void {
-    this.forEach((element) => {
-      element.removeBEMModifier(modifier);
-    });
-  }
+    /**
+     * Removes an event listener from all elements in the BEMNodeList
+     * @param type A case-sensitive string representing the event type to listen for.
+     * @param listener The callback that is to be removed
+     * @param useCapture A Boolean indicating whether events of this type will be dispatched to the registered listener before being dispatched to any EventTarget beneath it in the DOM tree.
+     */
+    public removeEventListener(type: string, listener: (event?: Event) => void, useCapture = false): void {
+        this.forEach((element) => {
+            element.removeEventListener(type, listener, useCapture);
+        });
+    }
 
-  /**
-   * Sets BEM modifier for one element of the BEMNodelist and remove it for the others
-   *
-   * @param modifier BEM modifier that needs to be set
-   * @param index Index of the element that needs the modifier to be added
-   * @param inverse Set modifier to all elements except for the one with index
-   */
-  public setBEMState(modifier: string, indexInput: number | number[]): void {
-    const indexes: number[] = Array.isArray(indexInput)
-      ? (indexInput as number[])
-      : ([indexInput] as number[]);
+    /**
+     * Toggles a BEM Modifier on or off for all elements of the BEMNodeList
+     */
+    public toggleBEMModifier(modifier: string, force?: boolean): void {
+        this.forEach((element) => {
+            element.toggleBEMModifier(modifier, force);
+        });
+    }
 
-    this.forEach((element, elementIndex) => {
-      const setModifier = indexes.indexOf(elementIndex) !== -1;
-      element.toggleBEMModifier(modifier, setModifier);
-    });
-  }
+    /**
+     * Adds a BEM Modifier for all elements of the BEMNodeList
+     */
+    public addBEMModifier(modifier: string): void {
+        this.forEach((element) => {
+            element.addBEMModifier(modifier);
+        });
+    }
+
+    /**
+     * Removes a BEM Modifier for all elements of the BEMNodeList
+     */
+    public removeBEMModifier(modifier: string): void {
+        this.forEach((element) => {
+            element.removeBEMModifier(modifier);
+        });
+    }
+
+    /**
+     * Sets BEM modifier for one element of the BEMNodelist and remove it for the others
+     *
+     * @param modifier BEM modifier that needs to be set
+     * @param index Index of the element that needs the modifier to be added
+     */
+    public setBEMState(modifier: string, indexInput: number | number[]): void {
+        const indexes: number[] = Array.isArray(indexInput) ? (indexInput as number[]) : ([indexInput] as number[]);
+
+        this.forEach((element, elementIndex) => {
+            const setModifier = indexes.indexOf(elementIndex) !== -1;
+            element.toggleBEMModifier(modifier, setModifier);
+        });
+    }
 }
--- src/bem-toolkit.bem-nodes.ts
+++ src/bem-toolkit.bem-nodes.ts
@@ -1,5 +1,5 @@
-import BEMNodeList from "./bem-toolkit.bem-nodelist";
-import "./bem-toolkit";
+import BEMNodeList from './bem-toolkit.bem-nodelist';
+import './bem-toolkit';
 
 /**
  * An associative array where the keys are BEM element names and the values are BEMNodeLists
@@ -7,140 +7,116 @@ import "./bem-toolkit";
  * Block elements without a BEM element name have the key '$bareBlock'
  */
 export default class BEMNodes {
-  [key: string]: BEMNodeList;
+    [key: string]: BEMNodeList;
 
-  public constructor(
-    target?: Element | NodeListOf<Element> | string,
-    mustInclude?: string[],
-    findRoot?: boolean
-  ) {
-    const elements = BEMNodes.getElementsFromTarget(target);
+    public constructor(target?: Element | NodeListOf<Element> | string, mustInclude?: string[], findRoot?: boolean) {
+        const elements = BEMNodes.getElementsFromTarget(target);
 
-    if (!elements) return;
+        if (!elements) return;
 
-    elements.forEach((elementNeedle) => {
-      const startElement = findRoot
-        ? elementNeedle.getBEMBlockRoot()
-        : elementNeedle;
+        elements.forEach((elementNeedle) => {
+            const startElement = findRoot ? elementNeedle.getBEMBlockRoot() : elementNeedle;
 
-      const blockName = startElement.getBEMBlockName();
+            const blockName = startElement.getBEMBlockName();
 
-      BEMNodes.getBlockElements(this, startElement as Element, blockName);
-    });
+            BEMNodes.getBlockElements(this, startElement as Element, blockName);
+        });
 
-    if (mustInclude) {
-      if (!BEMNodes.arrayContainsArray(Object.keys(this), mustInclude)) {
-        throw new Error("Not all required elements were found in the DOM");
-      }
-    }
-  }
-
-  public static create<T>(): BEMNodes {
-    return Object.create(BEMNodes.prototype) as BEMNodes;
-  }
-
-  /**
-   * Adds an element to the BEMNodes
-   *
-   * @param key BEM element name that the Element will be added to
-   * @param element Element that will be added to the BEMNodes
-   */
-  public static add(
-    bemnodesIn: BEMNodes,
-    key: string,
-    element: Element
-  ): BEMNodes {
-    const bemnodes = bemnodesIn;
-    if (typeof bemnodes[key] === "undefined") {
-      bemnodes[key] = new BEMNodeList(element);
-    } else {
-      try {
-        bemnodes[key].push(element);
-      } catch (err) {
-        if (typeof bemnodes[key] === "function") {
-          throw new Error(
-            `Could not add element with name '${key}' to this BEMNodes, because it is also a BEMNodes method`
-          );
-        } else {
-          throw new Error(err);
+        if (mustInclude) {
+            if (!BEMNodes.arrayContainsArray(Object.keys(this), mustInclude)) {
+                throw new Error('Not all required elements were found in the DOM');
+            }
         }
-      }
-    }
-    return bemnodes;
-  }
-
-  /**
-   *  Outputs array of Elements from different inputs
-   *
-   * @param target Target entrypoint of the BEMNodes
-   */
-  private static getElementsFromTarget(
-    target: string | Element | NodeListOf<Element>
-  ): Element[] {
-    if (typeof target === "string") {
-      return document.querySelectorAll(
-        target as string
-      ) as unknown as Element[];
-    }
-    if (target instanceof Element) {
-      return [target as Element];
     }
 
-    return target as unknown as Element[];
-  }
-
-  /**
-   * Returns a BEMNodes with all BEM elements in a block from a start Element
-   *
-   * @param accumulator Accumulates BEM block elements in a BEMNodes
-   * @param element Current element
-   * @param requiredBlockName Elements are ignored if they don't have this block name
-   */
-  private static getBlockElements(
-    accumulatorIn: BEMNodes,
-    element: Element,
-    requiredBlockName: string
-  ): BEMNodes {
-    let accumulator = accumulatorIn;
-    const elementName = element.getBEMElementName(requiredBlockName);
-    const blockName = element.getBEMBlockName(requiredBlockName);
-
-    let elementNameKey;
-    if (elementName)
-      elementNameKey = elementName.replace(/-([a-z])/g, (g) => {
-        return g[1].toUpperCase();
-      });
-
-    if (elementName && blockName === requiredBlockName) {
-      BEMNodes.add(accumulator, elementNameKey, element);
-    } else if (blockName === requiredBlockName) {
-      BEMNodes.add(accumulator, "$bareBlock", element);
+    public static create<T>(): BEMNodes {
+        return Object.create(BEMNodes.prototype) as BEMNodes;
     }
 
-    if (typeof element.children !== "undefined") {
-      [...element.children].forEach((childNode: Element) => {
-        accumulator = this.getBlockElements(
-          accumulator,
-          childNode,
-          requiredBlockName
-        );
-      });
+    /**
+     * Adds an element to the BEMNodes
+     *
+     * @param key BEM element name that the Element will be added to
+     * @param element Element that will be added to the BEMNodes
+     */
+    public static add(bemnodesIn: BEMNodes, key: string, element: Element, blockName: string): BEMNodes {
+        const bemnodes = bemnodesIn;
+        if (typeof bemnodes[key] === 'undefined') {
+            bemnodes[key] = new BEMNodeList(element);
+            bemnodes[key].blockName = blockName;
+        } else {
+            try {
+                bemnodes[key].push(element);
+            } catch (err) {
+                if (typeof bemnodes[key] === 'function') {
+                    throw new Error(
+                        `Could not add element with name '${key}' to this BEMNodes, because it is also a BEMNodes method`
+                    );
+                } else {
+                    throw new Error(err);
+                }
+            }
+        }
+        return bemnodes;
     }
-    return accumulator;
-  }
 
-  /**
-   * Checks whether every element of an array is inside another array
-   */
-  private static arrayContainsArray(
-    superset: string[],
-    subset: string[]
-  ): boolean {
-    if (subset.length === 0) {
-      return false;
+    /**
+     *  Outputs array of Elements from different inputs
+     *
+     * @param target Target entrypoint of the BEMNodes
+     */
+    private static getElementsFromTarget(target: string | Element | NodeListOf<Element>): Element[] {
+        if (typeof target === 'string') {
+            return document.querySelectorAll(target as string) as unknown as Element[];
+        }
+        if (target instanceof Element) {
+            return [target as Element];
+        }
+
+        return target as unknown as Element[];
     }
-    return subset.every((value) => {
-      return superset.indexOf(value) >= 0;
-    });
-  }
+
+    /**
+     * Returns a BEMNodes with all BEM elements in a block from a start Element
+     *
+     * @param accumulator Accumulates BEM block elements in a BEMNodes
+     * @param element Current element
+     * @param requiredBlockName Elements are ignored if they don't have this block name
+     */
+    private static getBlockElements(accumulatorIn: BEMNodes, element: Element, requiredBlockName: string): BEMNodes {
+        let accumulator = accumulatorIn;
+        const elementName = element.getBEMElementName(requiredBlockName);
+        const blockName = element.getBEMBlockName(requiredBlockName);
+
+        let elementNameKey;
+        if (elementName)
+            elementNameKey = elementName.replace(/-([a-z])/g, (g) => {
+                return g[1].toUpperCase();
+            });
+
+        if (elementName && blockName === requiredBlockName) {
+            BEMNodes.add(accumulator, elementNameKey, element, blockName);
+        } else if (blockName === requiredBlockName) {
+            BEMNodes.add(accumulator, '$bareBlock', element, blockName);
+        }
+
+        if (typeof element.children !== 'undefined') {
+            [...element.children].forEach((childNode: Element) => {
+                accumulator = this.getBlockElements(accumulator, childNode, requiredBlockName);
+            });
+        }
+        return accumulator;
+    }
+
+    /**
+     * Checks whether every element of an array is inside another array
+     */
+    private static arrayContainsArray(superset: string[], subset: string[]): boolean {
+        if (subset.length === 0) {
+            return false;
+        }
+        return subset.every((value) => {
+            return superset.indexOf(value) >= 0;
+        });
+    }
 }
